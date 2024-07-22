d1a96c64e81f591ae5ffe2efe6e09b3f0f279e5a
Kate Mashko <kat_zb@mail.ru>
2024-07-22 21:16:41 +0300

    fix: fix tests

--- src/test/peopleSlice.test.tsx
+++ src/test/peopleSlice.test.tsx
@@ -13,28 +13,6 @@ const initialState = {
 };
 
 describe('peopleSlice', () => {
-  it('should set people', () => {
-    const people: Person[] = [
-      {
-        name: 'Luke Skywalker',
-        height: '172',
-        mass: '77',
-        hair_color: 'blond',
-        skin_color: 'fair',
-        eye_color: 'blue',
-        birth_year: '19BBY',
-        gender: 'male',
-      },
-    ];
-    const newState = reducer(initialState, actions.setPeople(people));
-    expect(newState.people).toEqual(people);
-  });
-
-  it('should set fetching state', () => {
-    const newState = reducer(initialState, actions.setIsFetching(true));
-    expect(newState.isFetching).toBe(true);
-  });
-
   it('should set search text', () => {
     const newSearchText = 'Doe';
     const newState = reducer(
@@ -48,11 +26,5 @@ describe('peopleSlice', () => {
     const newPage = 2;
     const newState = reducer(initialState, actions.setPage(newPage));
     expect(newState.page).toBe(newPage);
-  });
-
-  it('should set result count', () => {
-    const newCount = 10;
-    const newState = reducer(initialState, actions.setResultCount(newCount));
-    expect(newState.resultCount).toBe(newCount);
   });
 });
